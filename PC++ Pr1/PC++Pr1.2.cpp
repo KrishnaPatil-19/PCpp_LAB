dfe7c8ae35543b77bbc6f8b87944483104628c5c
KrishnaPatil-19 <krishnaneetaanu@gmail.com>
2023-10-19 12:48:36 +0530

    first commit

---
+++ PC++ Pr1/PC++Pr1.2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i, sum = 0, avg;
+	for(i = 1; i <= 10; i++)
+	{
+		sum += i;
+		cout<<i<<"";
+	}
+	avg=sum / --i;
+	cout<<"\nSum = "<<sum;
+	cout<<"\nAverage = "<<avg;
+}
+
---
+++ PC++ Pr1/PC++Pr1.4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cout<<"\nEnter the value of A and B : ";
+	cin>>a>>b;
+	// Sum (+)
+	cout<<a<<" + "<<b<<" = "<<a+b<<endl;
+	// Subtraction (-)
+	cout<<a<<" - "<<b<<" = "<<a-b<<endl;
+	// Product (*)
+	cout<<a<<" * "<<b<<" = "<<a*b<<endl;
+	// Division/Quotient (/)
+	cout<<a<<" / "<<b<<" = "<<a/b<<endl;
+	return 0;
+}
+
